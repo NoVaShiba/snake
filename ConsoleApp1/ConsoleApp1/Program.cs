1d11927bdd1d0cfdf69bc6276af58ee82f6c74b1
  <v.shibalkovv.@mail.ru>
2017-03-25 12:08:19 +0400

    Функция Drow

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace snake
+namespace ConsoleApp1
 {
     class Program
     {
@@ -14,19 +14,21 @@ namespace snake
             int y1 = 3;
             char sym1 = '*';
 
-
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
+            Draw(x1, y1, sym1);
 
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
 
-
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
+            Draw(x2, y2, sym2);
 
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
--- snake/Program.cs
+++ snake/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace snake
+namespace ConsoleApp2
 {
     class Program
     {
@@ -15,8 +15,8 @@ namespace snake
             char sym1 = '*';
 
 
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
+            Console.SetCursorPosition( x1, y1 );
+            Console.Write( sym1 );
 
             int x2 = 4;
             int y2 = 5;
@@ -26,7 +26,14 @@ namespace snake
             Console.SetCursorPosition(x2, y2);
             Console.Write(sym2);
 
+
+
+
+
             Console.ReadLine();
+
+
+
         }
     }
 }
